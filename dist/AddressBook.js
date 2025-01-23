2980455b52d5cc7185c94fc4b320ff50345a281c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-23 21:37:24 +0530

    [Sparsh] Refactor: Updated Compiled Files

--- dist/AddressBook.js
+++ dist/AddressBook.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline_sync_1 = __importDefault(require("readline-sync"));
-const ClassAddressBook_1 = require("./ClassAddressBook");
+const Classes_1 = require("./Classes");
 // UC1 - Ability to Create a New Contact
 const createContact = () => {
     const firstName = readline_sync_1.default.question("Enter Your First Name: ");
@@ -38,9 +38,9 @@ const addMultipleContacts = () => {
     }
     return multipleContacts;
 };
-const addressBookFunction = () => {
-    console.log("Welcome to Address Book");
-    let addressBook = new ClassAddressBook_1.AddressBook();
+const addressBookFunction = (bookName) => {
+    console.log(`Welcome to ${bookName} Address Book`);
+    let addressBook = new Classes_1.AddressBook();
     while (true) {
         console.log("Operations: ");
         console.log("0: Get All Contacts");
@@ -48,6 +48,7 @@ const addressBookFunction = () => {
         console.log("2: Edit Contact");
         console.log("3: Delete Contact");
         console.log("4: Add Multiple Contacts");
+        console.log("9: Exit the Program");
         const operation = parseInt(readline_sync_1.default.question("Choose: "));
         switch (operation) {
             case 0:
@@ -69,7 +70,50 @@ const addressBookFunction = () => {
             case 4:
                 let multipleContacts = addMultipleContacts();
                 addressBook.addMultipleContacts(multipleContacts);
+                break;
+            case 9:
+                console.log(`Exiting ${bookName} Address Book...`);
+                return addressBook;
         }
     }
 };
-addressBookFunction();
+function addAddressBook(addressBookHandler) {
+    const nameOfAddressBook = readline_sync_1.default.question("Enter the Name of the Address Book: ");
+    const isDuplicate = addressBookHandler.getAllAddressBooks().some((book) => book.addressBookName === nameOfAddressBook);
+    if (isDuplicate) {
+        console.log(`An address book with the name "${nameOfAddressBook}" already exists. Cannot create a duplicate.`);
+        return;
+    }
+    console.log(`Creating a new address book: ${nameOfAddressBook}`);
+    const addressBook = new Classes_1.AddressBook();
+    const data = addressBookFunction(nameOfAddressBook);
+    const newAddressBook = {
+        addressBookName: nameOfAddressBook,
+        data: data.getAllContacts()
+    };
+    addressBookHandler.addAddressBook(newAddressBook);
+    console.log(`Address book "${nameOfAddressBook}" created successfully with data.`);
+}
+const addressBookManagerFunction = () => {
+    console.log("Welcome to the Address Book Manager");
+    let addressBookHandler = new Classes_1.AddressBookManager();
+    while (true) {
+        console.log("0: Get All Address Books");
+        console.log("1: Add Address Book");
+        console.log("9: Exit the Program");
+        const operation = parseInt(readline_sync_1.default.question("Choose: "));
+        switch (operation) {
+            case 0:
+                let allAddressBooks = addressBookHandler.getAllAddressBooks();
+                console.log(allAddressBooks);
+                break;
+            case 1:
+                addAddressBook(addressBookHandler);
+                break;
+            case 9:
+                console.log("Exiting the Address Book Manager...");
+                return;
+        }
+    }
+};
+addressBookManagerFunction();
