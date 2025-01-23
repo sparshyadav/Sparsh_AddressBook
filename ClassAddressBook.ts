e15343b78257f7cf05d020e53cdd3691f70f6b2f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-23 11:13:17 +0530

    [Sparsh] Refactor: Function to Edit Existing Contact Implemented

--- ClassAddressBook.ts
+++ ClassAddressBook.ts
@@ -1,3 +1,4 @@
+import readlineSync from "readline-sync";
 import { Contact } from "./ContactInterface";
 
 export class AddressBook {
@@ -14,6 +15,37 @@ export class AddressBook {
     addContant(contact: Contact): void {
         this.addressBook.push(contact);
         console.log("Contact Added Successfully");
+    }
+
+    editContact(contactName: string): void {
+        let objectToEdit = this.addressBook.find((obj) => {
+            return obj.firstName == contactName;
+        })
+
+        if (!objectToEdit) {
+            console.log("Contact Not Found");
+        }
+        else {
+            const firstName: string = readlineSync.question("Enter Your First Name: ");
+            const lastName: string = readlineSync.question("Enter Your Last Name: ");
+            const address: string = readlineSync.question("Enter Your Address: ");
+            const city: string = readlineSync.question("Enter Your City: ");
+            const state: string = readlineSync.question("Enter Your State: ");
+            const zip: number = parseInt(readlineSync.question("Enter Your ZIP Code: "));
+            const phoneNumber: number = parseInt(readlineSync.question("Enter Your Phone Number: "));
+            const email: string = readlineSync.question("Enter Your Email: ");
+
+            if (firstName) objectToEdit.firstName = firstName;
+            if (lastName) objectToEdit.lastName = lastName;
+            if (address) objectToEdit.address = address;
+            if (city) objectToEdit.city = city;
+            if (state) objectToEdit.state = state;
+            if (zip) objectToEdit.zip = zip;
+            if (phoneNumber) objectToEdit.phoneNumber = phoneNumber;
+            if (email) objectToEdit.email = email;
+
+            console.log("Contact Edited Successfully");
+        }
     }
 }
 
