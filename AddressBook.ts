de39e50585632e4e18d66ef3462da3b2594b13a4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-01-23 10:17:37 +0530

    [Sparsh] Add: UC1 - Ability to Create a Contact

---
+++ AddressBook.ts
@@ -0,0 +1,32 @@
+import readlineSync from "readline-sync";
+import { Contact } from "./ContactInterface";
+import { AddressBook } from "./ClassAddressBook";
+
+// UC1 - Ability to Create a New Contact
+const createContact = () => {
+    const firstName: string = readlineSync.question("Enter Your First Name: ");
+    const lastName: string = readlineSync.question("Enter Your Last Name: ");
+    const address: string = readlineSync.question("Enter Your Address: ");
+    const city: string = readlineSync.question("Enter Your City: ");
+    const state: string = readlineSync.question("Enter Your State: ");
+    const zip: number = parseInt(readlineSync.question("Enter Your ZIP Code: "));
+    const phoneNumber: number = parseInt(readlineSync.question("Enter Your Phone Number: "));
+    const email: string = readlineSync.question("Enter Your Email: ");
+
+    const newContact: Contact = {
+        firstName, lastName, address, city, state, zip, phoneNumber, email
+    }
+
+    return newContact;
+}
+
+const addressBookFunction = () => {
+    let addressBook = new AddressBook();
+    console.log("Welcome to Address Book");
+    console.log("Operations: ");
+    console.log("1: Add Contact");
+
+    const operation: number = parseInt(readlineSync.question("Choose: "));
+}
+
+addressBookFunction();
